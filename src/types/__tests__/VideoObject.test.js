8beee99d37a550095626f5e9300ab8eae58780cd
noelruiz34 <noelruiz34@gmail.com>
2025-08-06 10:40:11 -0500

    VideoObject test updates

--- src/types/__tests__/VideoObject.test.js
+++ src/types/__tests__/VideoObject.test.js
@@ -45,7 +45,18 @@ describe('VideoObjectValidator', () => {
     );
     const issues = await validator.validate(data);
     const errors = issues.filter((issue) => issue.severity === 'ERROR');
-    expect(errors.length).to.be.greaterThan(0);
+    const expectedErrors = [{
+        issueMessage: 'Required attribute "name" is missing',
+        severity: 'ERROR',
+      },
+      {
+        issueMessage: 'Required attribute "thumbnailUrl" is missing',
+        severity: 'ERROR',
+      },
+    ];
+    expect(errors).to.have.lengthOf(2);
+    expect(errors[0]).to.deep.include(expectedErrors[0]);
+    expect(errors[1]).to.deep.include(expectedErrors[1]);
   });
 
   it('should warn if neither contentUrl nor embedUrl is present', async () => {
@@ -85,5 +96,64 @@ describe('VideoObjectValidator', () => {
     const data = await loadTestData('VideoObject/valid2.json', 'jsonld');
     const issues = await validator.validate(data);
     expect(issues).to.have.lengthOf(0);
+  });
+
+  it('should validate a correct VideoObject structure in valid3.json', async () => {
+    const data = await loadTestData('VideoObject/valid3.json', 'jsonld');
+    const issues = await validator.validate(data);
+    const expectedIssues = [{
+        issueMessage: 'Missing field "expires" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "publication" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage:
+          'One of the following conditions needs to be met: Missing field "interactionStatistic" (optional) or Missing field "interactionCount" (optional)',
+        severity: 'WARNING',
+      },
+    ];
+    expect(issues).to.have.lengthOf(3);
+    expect(issues[0]).to.deep.include(expectedIssues[0]);
+    expect(issues[1]).to.deep.include(expectedIssues[1]);
+    expect(issues[2]).to.deep.include(expectedIssues[2]);
+  });
+
+  it('should validate a correct VideoObject structure in valid4.json', async () => {
+    const data = await loadTestData('VideoObject/valid4.json', 'jsonld');
+    const issues = await validator.validate(data);
+    const expectedIssues = [{
+        issueMessage: 'Missing field "duration" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "expires" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "hasPart" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "publication" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage:
+          'One of the following conditions needs to be met: Missing field "ineligibleRegion" (optional) or Missing field "regionsAllowed" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage:
+          'One of the following conditions needs to be met: Missing field "interactionStatistic" (optional) or Missing field "interactionCount" (optional)',
+        severity: 'WARNING',
+      },
+    ];
+    expect(issues).to.have.lengthOf(6);
+    for (let i = 0; i < issues.length; i++) {
+      expect(issues[i]).to.deep.include(expectedIssues[i]);
+    }
   });
 });
