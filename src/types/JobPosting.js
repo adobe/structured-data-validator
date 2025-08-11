175cc1067a74508dfc35f57b71355d4490f14968
noelruiz34 <noelruiz34@gmail.com>
2025-08-11 10:18:40 -0500

    feat: Add JobPosting

---
+++ src/types/JobPosting.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import BaseValidator from './base.js';
+
+export default class JobPostingValidator extends BaseValidator {
+  getConditions() {
+    const conditions = [
+      this.required('title', 'string'),
+      this.required('description', 'string'),
+      this.required('datePosted', 'date'),
+      this.required('hiringOrganization'),
+      this.conditionalRequirement(
+        'jobLocation',
+        'applicantLocationRequirements',
+      ), // if applicantLocationRequirements is present, jobLocation is not required
+
+      this.recommended('applicantLocationRequirements'),
+      this.recommended('baseSalary'),
+      this.recommended('directApply', 'boolean'),
+      this.recommended('employmentType'),
+      this.recommended('identifier'),
+      this.recommended('jobLocationType', 'string'),
+      this.recommended('validThrough', 'date'),
+    ];
+    return conditions.map((c) => c.bind(this));
+  }
+}
---
+++ src/types/__tests__/JobPosting.test.js
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { expect } from 'chai';
+import { Validator } from '../../validator.js';
+import { loadTestData } from './utils.js';
+
+describe('JobPostingValidator', () => {
+  let validator;
+  beforeEach(() => {
+    validator = new Validator();
+    validator.globalHandlers = [];
+  });
+
+  it('should validate a correct JobPosting structure in valid1.json', async () => {
+    const data = await loadTestData('JobPosting/valid1.json', 'jsonld');
+    const issues = await validator.validate(data);
+    const expectedIssues = [
+      {
+        issueMessage:
+          'Missing field "applicantLocationRequirements" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "directApply" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "jobLocationType" (optional)',
+        severity: 'WARNING',
+      },
+    ];
+    expect(issues).to.have.lengthOf(3);
+    for (let i = 0; i < 3; i++) {
+      expect(issues[i]).to.deep.include(expectedIssues[i]);
+    }
+  });
+
+  it('should validate a correct JobPosting structure for remote position in valid2.json', async () => {
+    const data = await loadTestData('JobPosting/valid2.json', 'jsonld');
+    const issues = await validator.validate(data);
+    expect(issues).to.have.lengthOf(1);
+    expect(issues[0]).to.deep.include({
+      issueMessage: 'Missing field "directApply" (optional)',
+      severity: 'WARNING',
+    });
+  });
+
+  it('should report errors for missing required fields', async () => {
+    const data = await loadTestData(
+      'JobPosting/missing-required.json',
+      'jsonld',
+    );
+    const issues = await validator.validate(data);
+    const expectedErrors = [
+      {
+        issueMessage: 'Required attribute "hiringOrganization" is missing',
+        severity: 'ERROR',
+      },
+      {
+        issueMessage: 'Required attribute "jobLocation" is missing',
+        severity: 'ERROR',
+      },
+    ];
+    const errors = issues.filter((issue) => issue.severity === 'ERROR');
+    expect(errors).to.have.lengthOf(expectedErrors.length);
+    for (let i = 0; i < expectedErrors.length; i++) {
+      expect(errors[i]).to.deep.include(expectedErrors[i]);
+    }
+  });
+
+  it('should warn for missing recommended fields', async () => {
+    const data = await loadTestData(
+      'JobPosting/missing-recommended.json',
+      'jsonld',
+    );
+    const issues = await validator.validate(data);
+    const expectedWarnings = [
+      {
+        issueMessage:
+          'Missing field "applicantLocationRequirements" (optional)',
+        severity: 'WARNING',
+      },
+      {
+        issueMessage: 'Missing field "directApply" (optional)',
+        severity: 'WARNING',
+      },
+    ];
+    const warnings = issues.filter((issue) => issue.severity === 'WARNING');
+    expect(warnings).to.have.lengthOf(expectedWarnings.length);
+    for (let i = 0; i < expectedWarnings.length; i++) {
+      expect(warnings[i]).to.deep.include(expectedWarnings[i]);
+    }
+  });
+});
