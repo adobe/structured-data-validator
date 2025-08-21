8ac3f7b3360e54cbbf4a4111238844b13dc16052
noelruiz34 <noelruiz34@gmail.com>
2025-08-21 11:16:38 -0500

    Only validate Offer if child of a Product

--- src/types/Offer.js
+++ src/types/Offer.js
@@ -13,17 +13,21 @@ import BaseValidator from './base.js';
 
 export default class PriceSpecificationValidator extends BaseValidator {
   getConditions() {
-    return [
-      this.or(
-        this.required('price', 'number'),
-        this.required('priceSpecification.price', 'number'),
-      ),
-      this.recommended('availability'),
-      this.or(
-        this.recommended('priceCurrency', 'currency'),
-        this.recommended('priceSpecification.priceCurrency', 'currency'),
-      ),
-      this.recommended('priceValidUntil', 'date'),
-    ].map((c) => c.bind(this));
+    const conditions = [];
+    if (this.path.length === 2 && this.path[0].type === 'Product') {
+      conditions.push(
+        this.or(
+          this.required('price', 'number'),
+          this.required('priceSpecification.price', 'number'),
+        ),
+        this.recommended('availability'),
+        this.or(
+          this.recommended('priceCurrency', 'currency'),
+          this.recommended('priceSpecification.priceCurrency', 'currency'),
+        ),
+        this.recommended('priceValidUntil', 'date'),
+      );
+    }
+    return conditions.map((c) => c.bind(this));
   }
 }
